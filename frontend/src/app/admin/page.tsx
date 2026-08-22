7894ff653da996b5c737860c5fb64044eece7e48
payammirzaei <payam.magic@gmail.com>
2026-08-22 09:38:36 +0200

    Provision API identity button

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,11 +1,9 @@
-import { AdminPasswordManager } from "@/components/admin-password-manager";
 import { MissionControl } from "@/components/mission-control";
 
 export default function AdminPage() {
   return (
     <>
       <MissionControl view="admin" />
-      <AdminPasswordManager />
     </>
   );
 }
